e722ec94948f9c034060024d4af73ebf98a2e918
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-01-30 22:19:34 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection')
+
 const app = express();
 
 dotenv.config({ path: 'config.env' })
@@ -11,6 +13,11 @@ const PORT = process.env.PORT || 8080;
 
 //log request
 app.use(morgan('tiny'));
+
+//mongodb connection
+
+connectDB();
+
 
 //parse request to parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,10 +30,7 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
-app.get('/add-user', (req, res) => {
-    res.render('add_user');
-})
+app.use('/', require('./server/routes/router'));
+
+
 app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
