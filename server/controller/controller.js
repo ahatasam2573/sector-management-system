1d2f36acc656b1543b66251fee29b5620d693374
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-01-31 02:28:06 +0600

    list of users added

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -36,13 +36,31 @@ exports.create = (req, res) => {
 //retrive all users
 exports.find = (req, res) => {
 
-    userDB.find()
-        .then(user => {
-            res.send(user)
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message })
-        })
+    if (req.query.id) {
+        const id = req.query.id;
+
+        userDB.findById(id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({ message: "Not found user with id " + id })
+                } else {
+                    res.send(data)
+                }
+            })
+            .catch(err => {
+                res.status(500).send({ message: "Erro retrieving user with id " + id })
+            })
+
+    } else {
+        userDB.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message: err.message || "Error Occurred while retriving user information" })
+            })
+    }
+
 
 }
 
