ca286c55b441f75389832e86878724a97ffaf275
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-01-30 22:30:52 +0600

    all routes and method added

---
+++ server/controller/controller.js
@@ -0,0 +1,22 @@
+const userDB = require('../model/model')
+
+//create and save users
+exports.create = (req, res) => {
+
+}
+
+//retrive all users
+exports.find = (req, res) => {
+
+}
+
+
+//update user
+exports.update = (req, res) => {
+
+}
+
+//delete user
+exports.delete = (req, res) => {
+
+}
